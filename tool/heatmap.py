e00d0429956a2a350cb6c7855171718be20082dc
Ronan Lamy <Ronan.Lamy@normalesup.org>
2010-03-18 19:23:21 +0000

    Fix bugs and position colorbar reasonably.

--- tool/heatmap.py
+++ tool/heatmap.py
@@ -25,14 +25,29 @@ class GridMap(Basemap):
         data: a 8000-long sequence of data-points corresponding to the subboxes
         returned by eqarea.grid8k, in the same order.
         """
-        data = np.asarray(data)
-        norm = matplotlib.colors.Normalize()
-        norm.autoscale(data)
+        data = np.asanyarray(data)
+        vmax, vmin = data.max(), data.min()
         for (i, box) in enumerate(the_grid):
             lons, lats = box.submesh()
             x, y = self(*np.meshgrid(lons, lats))
             try:
-                self.pcolor(x, y, data[i*100:(i+1)*100].reshape((10,10)), norm=norm)
+                self.pcolor(x, y, data[i*100:(i+1)*100].reshape((10,10)),
+                                                        vmax=vmax, vmin=vmin)
             except ValueError:  #pcolor raises if none of the subboxes is drawable
                 pass
-        plt.colorbar()
+        self.add_colorbar()
+
+    def add_colorbar(self, orientation=None):
+        """
+        Add a colorbar with specified orientation.
+
+        If the orientation isn't specified, choose vertical or horizontal based
+        on aspect ratio.
+        """
+        if orientation is None:
+            if self.aspect <0.6:
+                orientation = 'h'
+            else:
+                orientation = 'v'
+        plt.colorbar(orientation=orientation)
+
